46bc8e3364fc0d243f4316317ebb06eb0d2c74e1
sefcuoskar <89454750+sefcuoskar@users.noreply.github.com>
2022-01-26 19:27:46 +0100

    Update turtle_draw.py

--- turtle_draw.py
+++ turtle_draw.py
@@ -24,21 +24,21 @@ h.lt(135)
 
 
 seconds_count = 1; minute_count = 1; hour_count = 1
-t0 = time.time()
+# t0 = time.time()
 while True:
     
     s.fd(223.22)
     if seconds_count % 4 == 0:
         s.rt(96)
-        t1 = time.time()-t0
-        print(t1)
+        # t1 = time.time()-t0
+        # print(t1)
     else:
         s.rt(90)
     
     if seconds_count % 240 == 0:
         s.clear()
         minute_count = minute_count + 1
-        print(minute_count)
+        # print(minute_count)
         m.fd(155)
         m.rt(90)
         m.fd(155)
@@ -47,7 +47,7 @@ while True:
         m.rt(90)
         m.fd(155)
         m.rt(96)
-        t2 = time.time() - t0
+        # t2 = time.time() - t0
         print(t2)       
         if minute_count % 240 == 0:
             m.clear
